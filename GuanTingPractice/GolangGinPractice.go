c3a4b4d1979ee6a596e4ba6be6477ab001e91f3b
GuanTingFan <101400153+GuanTingFan@users.noreply.github.com>
2022-03-12 17:01:52 +0800

    gin

--- GuanTingPractice/GolangGinPractice.go
+++ GuanTingPractice/GolangGinPractice.go
@@ -1,5 +1,8 @@
 package main
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+)
 func main(){
 	router := gin.Default()
 	router.GET("./ping",func(c*gin.Context){
@@ -7,12 +10,26 @@ func main(){
 			"message":"Hello",
 		})
 	})
-	router.POST("./ping/:id",func(c*gin.Context){
-		id := c.Param("id")
+	// router.POST("./ping/:id",func(c*gin.Context){
+	// 	id := c.Param("id")
+	// 	c.JSON(200,gin.H{
+	// 		"id":id,
+	// 	})
+	// })
+
+	router.POST("./ping",func(c*gin.Context){
+		id := c.Query("id")
 		c.JSON(200,gin.H{
 			"id":id,
 		})
 	})
 	
+	router.POST("/",func(c *gin.Context){
+		body:=c.Request.Body
+		x, _ := ioutil.ReadAll(body)
+		c.JSON(200,gin.H{
+			"message":string(x),
+		})
+	})
 	router.Run()
 }  
