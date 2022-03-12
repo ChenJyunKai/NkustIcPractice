a6750c1555cd8de142115b51ffdc96da4b67a66c
GuanTingFan <101400153+GuanTingFan@users.noreply.github.com>
2022-03-12 15:16:45 +0800

    equation operation

--- GuanTingPratice/FirstGolang.go
+++ GuanTingPratice/FirstGolang.go
@@ -1,6 +1,77 @@
 package main
-import "fmt"
+import (
+	"fmt" 
+	"strings"
+	"strconv"
+)
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(equationprocessing("2*3-2/2*5"))
+} 
+
+func equationprocessing(equation string) int{
+	equation = strings.Replace(equation,"+"," + ", -1)
+	equation = strings.Replace(equation,"-"," - ", -1)
+	equation = strings.Replace(equation,"*"," * ", -1)
+	equation = strings.Replace(equation,"/"," / ", -1)
+	var splitarray []string = strings.Split(equation," ")
+	if strings.ContainsRune(equation, '*') ||  strings.ContainsRune(equation, '/') {
+		splitarray = strings.Split(muldiv(splitarray)," ") 
+	}
+	var sum int = 0
+	return Addsub(sum,splitarray)
 }
-  
+
+func Addsub(sum int,equation []string) int{
+	var i int
+	var preadd int
+	var err error
+	for i = 0; i < len(equation); i += 2{
+		preadd, err = strconv.Atoi(equation[i])
+		if err != nil {
+			fmt.Printf(err.Error())
+		}
+		if i == 0 {
+			sum = preadd
+		}else{
+			if equation[i-1] == "+"{
+				sum = sum + preadd
+			}else{
+				sum = sum - preadd
+			}
+		}	
+	}
+	return sum
+}
+
+func muldiv(equation []string) string{
+	var n,i int
+	var x,y int
+	var err error
+	var str string = ""
+	for i = 1; i < len(equation)-1 ; i += 2{
+		n = 0
+		x, err = strconv.Atoi(equation[i-1])
+		y, err = strconv.Atoi(equation[i+1])
+		if err != nil {
+			fmt.Printf(err.Error())
+		}
+		if equation[i] == "*"{
+			n = x * y
+			equation[i-1] = "";equation[i] = ""
+			equation[i+1] = strconv.Itoa(n)
+			
+		}else if equation[i] == "/"{
+			equation[i-1] = "";equation[i] = ""
+			n = x / y
+			equation[i+1] = strconv.Itoa(n)
+		}
+	}
+	for i = 0 ; i < len(equation); i++ {
+		if equation[i] != "" {
+			str = str + equation[i] + " "
+		}
+	}
+	str = strings.TrimSpace(str)
+	return str
+}
