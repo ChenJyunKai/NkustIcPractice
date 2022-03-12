c1560f4e965097b9490269d640ad885f2253f447
GuanTingFan <101400153+GuanTingFan@users.noreply.github.com>
2022-03-12 16:38:02 +0800

    gin update

---
+++ GuanTingPractice/GolangGinPractice.go
@@ -0,0 +1,18 @@
+package main
+import "github.com/gin-gonic/gin"
+func main(){
+	router := gin.Default()
+	router.GET("./ping",func(c*gin.Context){
+		c.JSON(200,gin.H{
+			"message":"Hello",
+		})
+	})
+	router.POST("./ping/:id",func(c*gin.Context){
+		id := c.Param("id")
+		c.JSON(200,gin.H{
+			"id":id,
+		})
+	})
+	
+	router.Run()
+}  
