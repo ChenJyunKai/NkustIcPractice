768838e92ccfb46c245fe74c9b89d0427862c13c
Uguarus <101421411+Uguarus@users.noreply.github.com>
2022-03-15 22:51:16 +0800

    Sorry

--- JunYiPractice/angular-tour-of-heroes/src/app/hamastars/hamastars.component.ts
+++ JunYiPractice/angular-tour-of-heroes/src/app/hamastars/hamastars.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hamastar } from '../hamastar';
 import { HAMASTARS } from '../mock-hamastars';
+import { HamastarService } from '../hamastar.service';
 
 @Component({
   selector: 'app-hamastars',
@@ -12,15 +13,20 @@ export class HamastarsComponent implements OnInit {
     id:1,
     name:"Junyi",
   }
+  hamastarss: Hamastar[] = [];
   selectedHamastar?: Hamastar;
   hamastars = HAMASTARS;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
   }
   OnSelect(hamastar:Hamastar):void{
     this.selectedHamastar=hamastar;
+  }
+
+  getHeroes(): void {
+    this.hamastarss = this.heroService.getHeroes();
   }
 
 }
